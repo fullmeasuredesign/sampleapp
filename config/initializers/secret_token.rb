0c5c82a95de34a7c77bba3366d96994006979edf
David Karasek <djk@fullmeasuredesign.com>
2013-05-19 12:19:29 -0500

    Initial commit, setup with RSpec testing.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = 'e9d17809e3d12c988b4da563fa4ebf53b0e553c37c3790779c58073ad6e25b3f63bb630f50849f41c810843b984ac5ec07b8c8bfae66fb4048c50a14950f942d'
